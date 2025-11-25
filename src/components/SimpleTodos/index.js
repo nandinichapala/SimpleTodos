24bd1ef5be5e8e72998906f59918586ef03859c6
nandinichapala <nandinichapala99@gmail.com>
2025-11-25 14:42:23 +0000

    pushing updated code

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,5 +1,5 @@
 import {Component} from 'react'
-
+import {v4 as uuidv4} from 'uuid'
 import './index.css'
 import TodoItem from '../TodoItem'
 
@@ -7,39 +7,47 @@ const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
+    completed: false,
   },
   {
     id: 2,
     title: 'Rent the movie for tomorrow movie night',
+    completed: false,
   },
   {
     id: 3,
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    completed: false,
   },
   {
     id: 4,
     title: 'Drop the parcel at Bloomingdale',
+    completed: false,
   },
   {
     id: 5,
     title: 'Order fruits on Big Basket',
+    completed: false,
   },
   {
     id: 6,
     title: 'Fix the production issue',
+    completed: false,
   },
   {
     id: 7,
     title: 'Confirm my slot for Saturday Night',
+    completed: false,
   },
   {
     id: 8,
     title: 'Get essentials for Sunday car wash',
+    completed: false,
   },
 ]
 
 class SimpleTodos extends Component {
-  state = {todosList: initialTodosList}
+  state = {todosList: initialTodosList, titleInput: ''}
 
   ondeleteTodo = id => {
     const {todosList} = this.state
@@ -47,18 +55,89 @@ class SimpleTodos extends Component {
     this.setState({todosList: filteredList})
   }
 
-  render() {
+  onClickAddNewTodo = async () => {
+    const {titleInput} = this.state
+    let taskTitle = titleInput
+
+    const count = parseInt(titleInput.charAt(titleInput.length - 1))
+
+    let countInput = 1
+    if (Number.isInteger(count) && count > countInput) {
+      countInput = count
+      taskTitle = titleInput.slice(0, -1)
+    }
+
+    for (let i = 0; i < countInput; i++) {
+      const newTodo = {
+        id: uuidv4(),
+        title: taskTitle,
+        completed: false,
+      }
+      this.setState(prevState => ({
+        todosList: [...prevState.todosList, newTodo],
+        titleInput: '',
+      }))
+    }
+  }
+
+  onChangeTitleInput = event => {
+    this.setState({titleInput: event.target.value})
+  }
+
+  onToggleCompleted = id => {
     const {todosList} = this.state
+    const updatedList = todosList.map(eachTodo =>
+      eachTodo.id === id
+        ? {...eachTodo, completed: !eachTodo.completed}
+        : eachTodo,
+    )
+    this.setState({todosList: updatedList})
+  }
+
+  onUpdateTitle = (updatedTitle,id) => {
+    const {todosList}=this.state
+    const updatedList=todosList.map((each)=>{
+      if (each.id===id){
+        return {...each,title:updatedTitle}
+      }
+      return each
+    }
+  )
+  this.setState({todosList:updatedList})
+  
+  }
+
+  render() {
+    const {todosList, titleInput} = this.state
+
     return (
       <div className="container">
         <div className="box-container">
           <h1 className="heading">Simple Todos</h1>
+          <div className="title input-add-btn-container">
+            <input
+              type="text"
+              className="title-input"
+              value={titleInput}
+              onChange={this.onChangeTitleInput}
+              placeholder="Add Tasks"
+            />
+            <button
+              type="button"
+              className="add-btn"
+              onClick={this.onClickAddNewTodo}
+            >
+              Add
+            </button>
+          </div>
           <ul className="list-container">
             {todosList.map(eachObj => (
               <TodoItem
                 todoDetails={eachObj}
                 key={eachObj.id}
                 ondeleteTodo={this.ondeleteTodo}
+                onToggleCompleted={this.onToggleCompleted}
+                onUpdateTitle={this.onUpdateTitle}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,20 +1,94 @@
 import './index.css'
+import {Component} from 'react'
 
-const TodoItem = props => {
-  const {todoDetails, ondeleteTodo} = props
-  const {title, id} = todoDetails
-  const onClickDeleteTodo = () => {
-    ondeleteTodo(id)
+class TodoItem extends Component {
+  state = {editing: false, updatedTitle: ''}
+
+  onClickDeleteTodo = () => {
+    const {todoDetails, ondeleteTodo} = this.props
+    ondeleteTodo(todoDetails.id)
   }
-  return (
-    <li className="list-item">
-      <p className="content">{title}</p>
-      <button className="delete-button" onClick={onClickDeleteTodo}>
-        {' '}
-        Delete
-      </button>
-    </li>
-  )
+
+  onChangeActivityStatus = () => {
+    const {todoDetails, onToggleCompleted} = this.props
+    onToggleCompleted(todoDetails.id)
+  }
+
+  onClickEditBtn = () => {
+    const {todoDetails} = this.props
+    this.setState(prevState => ({
+      editing: !prevState.editing,
+      updatedTitle: todoDetails.title,
+    }))
+  }
+
+  onClickSaveBtn = () => {
+    const {todoDetails,onUpdateTitle} = this.props
+    const {updatedTitle} = this.state
+    this.setState(prevState => ({editing: !prevState.editing}))
+    onUpdateTitle(updatedTitle,todoDetails.id)
+  }
+
+  onChangeTitleText = event => {
+    this.setState({updatedTitle: event.target.value})
+  }
+
+  render() {
+    const {todoDetails} = this.props
+    const {completed} = todoDetails
+    const {updatedTitle, editing} = this.state
+
+    console.log(updatedTitle)
+    const textEffects = completed ? 'text-effects' : ''
+    return (
+      <>
+        {editing ? (
+          <li className="list-item">
+            <input
+              type="text"
+              className="editsble-input"
+              value={updatedTitle}
+              onChange={this.onChangeTitleText}
+            />
+            <button
+              type="button"
+              className="save-btn"
+              onClick={this.onClickSaveBtn}
+            >
+              Save
+            </button>
+          </li>
+        ) : (
+          <li className="list-item">
+            <div className="check-box-title-text-container">
+              <input
+                type="checkbox"
+                checked={completed}
+                onChange={this.onChangeActivityStatus}
+              />
+              <p className={`content ${textEffects}`}>{todoDetails.title}</p>
+            </div>
+            <div className="edit-btn-del-btn-container">
+              <button
+                type="button"
+                className="edit-btn"
+                onClick={this.onClickEditBtn}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="delete-button"
+                onClick={this.onClickDeleteTodo}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        )}
+      </>
+    )
+  }
 }
 
 export default TodoItem
